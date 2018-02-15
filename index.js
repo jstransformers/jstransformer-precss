7401cd48e5e62255ca7b33248c8765748cc4624a
Rob Loach <robloach@gmail.com>
2018-02-15 10:42:56 -0500

    Update Dependencies

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 'use strict'
 
 const precss = require('precss')
-const scss = require('postcss-scss')
 const extend = require('extend-shallow')
 
 exports.name = 'precss'
@@ -9,10 +8,9 @@ exports.outputFormat = 'css'
 
 exports.renderAsync = function (str, options, locals) {
   return new Promise((resolve, reject) => {
-    extend(options, locals)
-    precss(options).process(str, {
-      parser: scss
-    }).then(result => {
+    const opts = extend({}, options, locals)
+    // TODO: Add opts.from to allow correct source maps.
+    precss.process(str, opts).then(result => {
       resolve(result.css)
     }).catch(reject)
   })
